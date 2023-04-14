edf1f885b7136994c01346fa45be9a1b3055c4ce
Carolina Martinez <caromarmachi@gmail.com>
2023-04-14 12:02:26 +0200

    - Creation du fichier javascript et des 3 methodes d'affichage/masquage

--- jscript/main.js
+++ jscript/main.js
@@ -1,3 +1,27 @@
 /**
  * 
  */
+ function affichagePage2()
+ {
+	 	document.getElementById('NouveauLivre').style.display='block';
+		document.getElementById('NouveauLivre').style.visibility='visible';
+		document.getElementById('myBooks').style.visibility='hidden';
+		document.getElementById('myBooks').style.display='none';
+		document.getElementById('pochListe').style.visibility='visible';
+ }
+ 
+  function affichageResultat1()
+ {
+		document.getElementById('ResultatLivre').style.visibility='visible';
+		document.getElementById('pochListe').style.visibility='hidden';
+ }
+ 
+   function annulerRecherche()
+ {
+	 	document.getElementById('NouveauLivre').style.display='none';
+		document.getElementById('NouveauLivre').style.visibility='hidden';
+		document.getElementById('myBooks').style.visibility='visible';
+		document.getElementById('myBooks').style.display='block';
+		document.getElementById('pochListe').style.visibility='visible';
+		document.getElementById('ResultatLivre').style.visibility='hidden';
+ }
